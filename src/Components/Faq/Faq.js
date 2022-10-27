f64f5d1e0930d7e6a1f18a52e19abd2320b355b6
sadnanrakib <sadnanrakib02@gmail.com>
2022-10-27 22:17:19 +0600

    Final Commit

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react';
-import { AuthContext } from '../../Context/AuthProvider';
+
 
 const Faq = () => {
-    // const {user} = useContext(AuthContext)
+    
     
     return (
         <div>
-           <h2>FAQ is i a</h2> 
+           <h2>FAQ is  a</h2> 
         </div>
     );
 };
